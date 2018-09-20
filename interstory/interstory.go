b1b8c80ddcd3dc3b08978217f33d363a3812a73e
Kevin Sarmiento <kevin.sarmiento@uruit.com>
2018-09-20 13:07:33 -0500

    Created Handler and structs to modelate business.

--- interstory/interstory.go
+++ interstory/interstory.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"errors"
 	"io/ioutil"
+	"net/http"
+	"strings"
+	"fmt"
 )
 
 type Arc struct {
@@ -12,19 +15,38 @@ type Arc struct {
 	Options []map[string]string
 }
 
-func NewStory(file string) (map[string]Arc, error) {
+type Story struct {
+	Arcs map[string]Arc
+}
+
+type StoryHandler struct {
+	Story *Story
+}
+
+func (sh StoryHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL.Path)
+	path := strings.TrimLeft(r.URL.Path, "/")
+	arc, exists := sh.Story.Arcs[path]
+	if exists {
+		fmt.Println(arc.Title)
+		fmt.Fprintln(w, arc.Title)
+	}
+}
+
+func NewStory(file string) (*Story, error) {
 	introArcName := "intro"
 	storyFile, err := ioutil.ReadFile(file)
 	if err != nil {
 		return nil, err
 	}
+	
+	var arcs map[string]Arc
+	json.Unmarshal(storyFile, &arcs)
 
-	var story map[string]Arc
-	json.Unmarshal(storyFile, &story)
-
-	if _, introExists := story[introArcName]; !introExists {
+	if _, introExists := arcs[introArcName]; !introExists {
 		return nil, errors.New("There's no intro for this story")
 	}
 
+	story := &Story{Arcs: arcs}
 	return story, nil
 }
--- main.go
+++ main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"fmt"
 	"interactive-book-golang/interstory"
+	"net/http"
 )
 
 func main() {
+	port := "7000"
 	story, err := interstory.NewStory("story.json")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(story)
+
+	storyHandler := interstory.StoryHandler{Story: story}
+	mux := http.NewServeMux()
+	mux.Handle("/", storyHandler)
+	fmt.Println("Listening at port " + port + "...")
+	http.ListenAndServe(":" + port, mux)
 }
